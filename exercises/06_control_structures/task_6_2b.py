2775652770e52eb4fcac2b500d10497f45e3f44f
kp0iii <krasheninnikov.pavel@gmail.com>
2022-01-28 03:28:23 +0700

    task_6_2b-done

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+i = 1
+
+while i == 1:
+    ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+    if ip.replace('.', '').isnumeric() == False or ip.count('.') != 3:
+        print('Неправильный IP-адрес')
+    elif ip.replace('.', '').isnumeric() and (0 < int(ip.split('.')[0]) > 255 or 0 < int(ip.split('.')[1]) > 255 \
+            or 0 < int(ip.split('.')[2]) > 255 or 0 < int(ip.split('.')[3]) > 255):
+        print('Неправильный IP-адрес')
+    else:
+        if 1 <= int(ip.split('.')[0]) <= 223:
+            print('unicast')
+        elif 224 <= int(ip.split('.')[0]) <= 239:
+            print('multicast')
+        elif ip == '255.255.255.255':
+            print('local broadcast')
+        elif ip == '0.0.0.0':
+            print('unassigned')
+        else:
+            print('unused')
+        break
