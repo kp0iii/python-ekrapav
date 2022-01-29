bdd9316b81b9133f2c3de2e95ef0f796ddf4f5c5
kp0iii <krasheninnikov.pavel@gmail.com>
2022-01-30 02:54:17 +0700

    task_9_1a-done

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -43,3 +43,31 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+
+def generate_access_config(intf_vlan_mapping, access_template, port_security=None):
+    """
+    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+        {'FastEthernet0/12':10,
+         'FastEthernet0/14':11,
+         'FastEthernet0/16':17}
+    access_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    result = []
+    for intf, vlan in intf_vlan_mapping.items():
+        result.append('interface ' + intf)
+        for command in access_template:
+            if command.endswith('vlan'):
+                result.append(command + ' ' + str(vlan))
+            else:
+                result.append(command)
+        if port_security:
+            for command in port_security:
+                result.append(command)
+    return result
+
+
+print(generate_access_config(access_config, access_mode_template, port_security_template))
+
