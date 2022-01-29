c6d14ca2408eb0151076d0047a310c3977e01542
kp0iii <krasheninnikov.pavel@gmail.com>
2022-01-30 03:38:57 +0700

    task_9_2a-done

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -35,7 +35,6 @@
 
 """
 
-
 trunk_mode_template = [
     "switchport mode trunk",
     "switchport trunk native vlan 999",
@@ -47,3 +46,49 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+
+def generate_trunk_config(trunk_config_template, trunk_mode_template):
+    """
+    - intf_vlan_mapping: ожидает как аргумент словарь с соответствием интерфейс-VLANы
+    такого вида:
+{
+    "FastEthernet0/1": [
+        "switchport mode trunk",
+        "switchport trunk native vlan 999",
+        "switchport trunk allowed vlan 10,20,30",
+    ],
+    "FastEthernet0/2": [
+        "switchport mode trunk",
+        "switchport trunk native vlan 999",
+        "switchport trunk allowed vlan 11,30",
+    ],
+    "FastEthernet0/4": [
+        "switchport mode trunk",
+        "switchport trunk native vlan 999",
+        "switchport trunk allowed vlan 17",
+    ],
+}
+    - trunk_template: ожидает как аргумент шаблон конфигурации trunk-портов в виде
+    списка команд (список trunk_mode_template)
+
+    Функция должна возвращать список команд с конфигурацией на основе указанных портов
+    и шаблона trunk_mode_template. В конце строк в списке не должно быть символа
+    перевода строки.
+    """
+
+    result = {}
+    result_list = []
+    for intf, vlan in trunk_config_template.items():
+        for command in trunk_mode_template:
+            if command.endswith('vlan'):
+                result_list.append(command + ' ' + str(vlan).replace(' ', '').strip('[]'))
+            else:
+                result_list.append(command)
+            result[intf] = result_list
+        result_list = []
+    return result
+
+
+result_main = generate_trunk_config(trunk_config, trunk_mode_template)
+print(result_main)
