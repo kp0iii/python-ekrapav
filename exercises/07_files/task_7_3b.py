c2f501feeee9669c816caa587938eac455a19e5d
kp0iii <krasheninnikov.pavel@gmail.com>
2022-01-30 00:20:03 +0700

    task_7_3b-done

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,21 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+vlan_req = input('Введите номер VLAN: ')
+
+file_name_r = 'CAM_table.txt'
+
+result = []
+k = 0
+
+with open(file_name_r) as f_read:
+    for line in f_read:
+        if line.strip() and line.strip()[0].split()[-1][-1].isdigit():
+            result.append(line.split())
+            result[k][0] = int(result[k][0])
+            k += 1
+result.sort()
+for i in result:
+    vlan, mac, _, intf = i
+    if vlan == int(vlan_req):
+        print('{:<7}''{:<17}''{:<5}'.format(vlan, mac, intf))
