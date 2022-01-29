e59462a8bb6f8ed9217c6cce3783d361ffbb2419
kp0iii <krasheninnikov.pavel@gmail.com>
2022-01-30 04:27:24 +0700

    task_9_3-done

--- exercises/09_functions/task_9_3.py
+++ exercises/09_functions/task_9_3.py
@@ -23,3 +23,39 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+
+def get_int_vlan_map(config_filename):
+    """
+    функцию get_int_vlan_map, которая обрабатывает конфигурационный
+    файл коммутатора и возвращает кортеж из двух словарей:
+    * словарь портов в режиме access, где ключи номера портов,
+      а значения access VLAN (числа):
+    {'FastEthernet0/12': 10,
+     'FastEthernet0/14': 11,
+     'FastEthernet0/16': 17}
+
+    * словарь портов в режиме trunk, где ключи номера портов,
+      а значения список разрешенных VLAN (список чисел):
+    {'FastEthernet0/1': [10, 20],
+     'FastEthernet0/2': [11, 30],
+     'FastEthernet0/4': [17]}
+    """
+    access = {}
+    trunk = {}
+    with open(config_filename, 'r') as f:
+        for line in f:
+            if line.startswith('interface'):
+                intf = line.rstrip().split()[-1]
+            elif line.strip().startswith('switchport access'):
+                vlan = int(line.split()[-1])
+                access[intf] = vlan
+            elif line.strip().startswith('switchport trunk allowed vlan'):
+                vlan = line[31:].rstrip().split(',')
+                vlan_result_trunk = [int(item) for item in vlan]
+                trunk[intf] = list(vlan_result_trunk)
+        print(access)
+        print(trunk)
+
+
+get_int_vlan_map('config_sw1.txt')
