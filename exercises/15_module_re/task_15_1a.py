f97aeb51b5a548f03d30f8aca74917104a18f6da
kp0iii <krasheninnikov.pavel@gmail.com>
2022-02-06 01:28:32 +0700

    task_15_1a-done

--- exercises/15_module_re/task_15_1a.py
+++ exercises/15_module_re/task_15_1a.py
@@ -24,3 +24,27 @@
 а не ввод пользователя.
 
 """
+
+
+import re
+from pprint import pprint
+
+
+def get_ip_from_cfg(filename):
+    regex = re.compile(r'.ip address ((?P<ip>\d+.){3}\d) ((?P<mask>\d+.){3}\d+)')
+    regex_intf = re.compile(r'^interface (?P<interface>\S+)\n')
+    result = {}
+    with open(filename) as f:
+        for line in f:
+            match = re.search(regex, line)
+            match1 = re.search(regex_intf, line)
+            if match:
+                result[result_intf] = match.group(1, 3)
+            elif match1:
+                result_intf = match1.group(1)
+    return result
+
+
+if __name__ == "__main__":
+    final_result = get_ip_from_cfg('config_r1.txt')
+    pprint(final_result)
